65e801bc1f57c68ad755f80903dd022e27f3364c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-04-01 23:05:57 +0700

    :money_with_wings: del express

--- backend/src/config/db.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import config from "./config";
-
-const firebaseApp = () => {
-  return initializeApp(config.firebaseConfig);
-};
-
-export default firebaseApp;
