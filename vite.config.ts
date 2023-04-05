4c9be21d22cb9bad42e17533a85842fe5a97ef46
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-04-06 00:00:40 +0700

    :money_with_wings: setup env && plungins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+
+const env = dotenv.config().parsed;
+const envApi = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: envApi,
 });
